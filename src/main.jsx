77cb9edc33465d540599e0c513b505f2d05f220a
Rezu0 <yunianrezky031@gmail.com>
2024-08-11 23:55:55 +0700

    config primereact provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async';
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
 import App from './App.jsx';
 import 'react-loading-skeleton/dist/skeleton.css';
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+
 
 ReactDOM.createRoot(
   document.getElementById('root')
