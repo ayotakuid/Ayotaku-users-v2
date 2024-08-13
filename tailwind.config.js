00eb28ac9e17d433d69fc34fb6c665a28361dc5d
Rezu0 <yunianrezky031@gmail.com>
2024-08-13 17:18:46 +0700

    config untuk primeicons dan tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+      './index.html',
+      './src/**/*.{vue,js,ts,jsx,tsx}',
+      './node_modules/primereact/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+      extend: {
+        backgroundColor: {
+          "ayotaku-box": "#46496A",
+        },
+        textColor: {
+          "ayotaku-text-default": "#D9D2FF",
+        },
+      },
+  },
+  plugins: [],
+};
