3e52c0331b43a2b5167c2e8f550900b7cd216e2a
Rezu0 <yunianrezky031@gmail.com>
2024-08-13 17:30:03 +0700

    config primereact provider ke tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async';
 import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+import Tailwind from 'primereact/passthrough/tailwind';
+
+// IMPORT FILE STATIC
 import App from './App.jsx';
+
+// IMPORT STYLING
 import 'react-loading-skeleton/dist/skeleton.css';
 import "primereact/resources/themes/lara-light-cyan/theme.css";
+import './style/global.css';
 
-
+// IMPORT ICONS
+import 'primeicons/primeicons.css';
+        
 ReactDOM.createRoot(
   document.getElementById('root')
 ).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
+    <PrimeReactProvider value={{ pt: Tailwind }}>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </PrimeReactProvider>
   </React.StrictMode>,
 )
