ab6b5a2a440ad4d289af798a9d3f75f659564968
Rezu0 <yunianrezky031@gmail.com>
2024-08-16 02:47:58 +0700

    ubah sedikit untuk loading skeleton register

--- src/auth/RegisterComponent.jsx
+++ src/auth/RegisterComponent.jsx
@@ -71,14 +71,14 @@ function RegisterComponent() {
                   highlightColor="#e0e0e0" 
                   duration={2}
                 >
-                  <Skeleton circle={true} width={64} height={64} />
+                  <Skeleton circle={true} width={40} height={40} />
                 </SkeletonTheme>
                 : <img src={IconCirleAyotaku} alt="Icon Circle Ayotaku" className="w-10" />
               }
             </div>
             <h1 className="text-2xl grid items-center justify-center text-ayotaku-text-default mt-5">Sign Up</h1>
             <div className="grid items-center justify-center">
-              <span className="grid items-center justify-center text-xs text-teal-400 text-opacity-70 w-96 text-center mt-3">
+              <span className="grid grid-cols-1 items-center justify-center text-xs text-teal-400 text-opacity-70 w-96 text-center mt-3">
                 Enter your email, username and password below to create account or you can register via Google.
                 </span>
             </div>
@@ -99,12 +99,12 @@ function RegisterComponent() {
                         duration={2}
                       >
                         <Skeleton width={50} height={10} />
-                        <Skeleton width={270} height={30} />
+                        <Skeleton width={270} height={35} />
                         <Skeleton width={50} height={10} className="mt-5"/>
-                        <Skeleton width={270} height={30} />
+                        <Skeleton width={270} height={35} />
                         <Skeleton width={50} height={10} className="mt-5"/>
-                        <Skeleton width={270} height={30} />
-                        <Skeleton width={270} height={30} className="mt-5"/>
+                        <Skeleton width={270} height={35} />
+                        <Skeleton width={270} height={35} className="mt-5"/>
                         <Skeleton width={100} height={10} className="mt-5 text-center" />
                       </SkeletonTheme>
                     </div>
@@ -176,21 +176,37 @@ function RegisterComponent() {
               <div className="p-4 flex bg-ayotaku-box rounded-lg justify-center items-center col-span-12 md:col-span-5 sm:col-span-5">
                 <div className="grid grid-cols-1">
 
-                <Button 
-                  className="flex items-center justify-center w-full px-4 py-2 dark:bg-white border border-gray-400 shadow-sm dark:text-black dark:hover:bg-gray-100 text-sm rounded-full"
-                  onClick={handlerSignUpGoogle}
-                  loading={isLoadingButtonGoogle}
-                >
-                  {/* ICON SVG Google */}
-                  <svg className="w-6 h-6 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
-                    <path fill="#EA4335" d="M24 9.5c3.5 0 6.4 1.2 8.8 3.5l6.5-6.5C34.4 2.5 29.5 0 24 0 14.6 0 6.7 5.8 3 14.1l7.7 6.3C12.3 13.3 17.7 9.5 24 9.5z"></path>
-                    <path fill="#4285F4" d="M46.9 24.6c0-1.7-.1-2.9-.3-4.3H24v8.1h13c-.5 2.4-1.7 4.4-3.5 5.8l7.7 6c4.4-4.1 6.7-10.1 6.7-15.6z"></path>
-                    <path fill="#FBBC05" d="M10.9 28.7c-1.2-2.4-1.9-5-1.9-7.7s.7-5.3 1.9-7.7l-7.7-6.3C2.3 11.3 0 17.4 0 24s2.3 12.7 6.3 17.3l7.6-6.3z"></path>
-                    <path fill="#34A853" d="M24 48c6.5 0 11.9-2.1 15.8-5.7l-7.7-6.1c-2.1 1.5-4.9 2.5-8 2.5-6.3 0-11.6-4.2-13.4-10.1l-7.7 6C6.8 42.1 14.7 48 24 48z"></path>
-                    <path fill="none" d="M0 0h48v48H0z"></path>
-                  </svg>
-                  Sign Up with Google
-                </Button>
+                  {
+                    (!isLoadingPage)
+                    ?
+                    <>
+                      <SkeletonTheme
+                        baseColor="#c0c0c0"
+                        highlightColor="#e0e0e0"
+                        duration={2}
+                      >
+                        <Skeleton width={195} height={50} />
+                      </SkeletonTheme>
+                    </>
+                    :
+                    <>
+                      <Button 
+                        className="flex items-center justify-center w-full px-4 py-2 dark:bg-white border border-gray-400 shadow-sm dark:text-black dark:hover:bg-gray-100 text-sm rounded-full"
+                        onClick={handlerSignUpGoogle}
+                        loading={isLoadingButtonGoogle}
+                      >
+                        {/* ICON SVG Google */}
+                        <svg className="w-6 h-6 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+                          <path fill="#EA4335" d="M24 9.5c3.5 0 6.4 1.2 8.8 3.5l6.5-6.5C34.4 2.5 29.5 0 24 0 14.6 0 6.7 5.8 3 14.1l7.7 6.3C12.3 13.3 17.7 9.5 24 9.5z"></path>
+                          <path fill="#4285F4" d="M46.9 24.6c0-1.7-.1-2.9-.3-4.3H24v8.1h13c-.5 2.4-1.7 4.4-3.5 5.8l7.7 6c4.4-4.1 6.7-10.1 6.7-15.6z"></path>
+                          <path fill="#FBBC05" d="M10.9 28.7c-1.2-2.4-1.9-5-1.9-7.7s.7-5.3 1.9-7.7l-7.7-6.3C2.3 11.3 0 17.4 0 24s2.3 12.7 6.3 17.3l7.6-6.3z"></path>
+                          <path fill="#34A853" d="M24 48c6.5 0 11.9-2.1 15.8-5.7l-7.7-6.1c-2.1 1.5-4.9 2.5-8 2.5-6.3 0-11.6-4.2-13.4-10.1l-7.7 6C6.8 42.1 14.7 48 24 48z"></path>
+                          <path fill="none" d="M0 0h48v48H0z"></path>
+                        </svg>
+                        Sign Up with Google
+                      </Button>
+                    </>
+                  }
 
                 </div>
               </div>
