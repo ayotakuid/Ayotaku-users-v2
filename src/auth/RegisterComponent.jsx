a60341b933774d08c89608cb518f74f51381967f
Rezu0 <yunianrezky031@gmail.com>
2024-08-15 20:24:06 +0700

    add button sign up google

--- src/auth/RegisterComponent.jsx
+++ src/auth/RegisterComponent.jsx
@@ -1,18 +1,21 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Button } from "primereact/button";
 import { Divider } from "primereact/divider";
 import { Helmet } from "react-helmet-async";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
+// IMPORT UTILS
+import { URL_API_AYOTAKU } from '../utils/secrets.json';
 
 // IMPORT IMAGE
 import IconCirleAyotaku from '../image/icon-circle.svg';
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 
 function RegisterComponent() {
   const navigate = useNavigate();
 
   const [isLoadingPage, setIsLoadingPage] = useState(false);
+  const [isLoadingButtonGoogle, setIsLoadingButtonGoogle] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
@@ -24,10 +27,18 @@ function RegisterComponent() {
     navigate('/');
   }
 
+  const handlerSignUpGoogle = () => {
+    const url = `${URL_API_AYOTAKU}/user/auth/google`;
+    const windowName = 'Ayotaku id - Sign up with Google'
+    const windowSize = 'width=500,height=500,left=100,top=100';
+
+    window.open(url, windowName, windowSize);
+  }
+
   return (
     <>
       <Helmet>
-        <title>Ayotaku id - Register</title>
+        <title>Ayotaku id - Sign up</title>
         <meta name="description" content="Selamat datang di Ayotaku.id! Ini adalah Website untuk Streaming Anime dan Downloadn Anime secara gratis. Kami ingin mempermudah para Otaku ataupun wibu diluar sana yang kesulitan untuk membeli Subscription Anime Legal ataupun Website sebelah yang penuh dengan iklan!"/>
       </Helmet>
 
@@ -70,12 +81,14 @@ function RegisterComponent() {
                         highlightColor="#e0e0e0"
                         duration={2}
                       >
-                        <Skeleton width={70} height={10} />
-                        <Skeleton width={270} height={40} />
-                        <Skeleton width={70} height={10} className="mt-5"/>
-                        <Skeleton width={270} height={40} />
-                        <Skeleton width={70} height={10} className="mt-5"/>
-                        <Skeleton width={270} height={40} />
+                        <Skeleton width={50} height={10} />
+                        <Skeleton width={270} height={30} />
+                        <Skeleton width={50} height={10} className="mt-5"/>
+                        <Skeleton width={270} height={30} />
+                        <Skeleton width={50} height={10} className="mt-5"/>
+                        <Skeleton width={270} height={30} />
+                        <Skeleton width={270} height={30} className="mt-5"/>
+                        <Skeleton width={100} height={10} className="mt-5 text-center" />
                       </SkeletonTheme>
                     </div>
                   </>
@@ -112,9 +125,9 @@ function RegisterComponent() {
                       <Button 
                         label="Sign up"
                         // icon="pi pi-user-plus"
-                        className="mt-4 w-full dark:bg-ayotaku-button dark:hover:bg-slate-400 outline-none"
+                        className="mt-4 w-full dark:text-white dark:bg-ayotaku-button dark:hover:bg-slate-400 outline-none"
                         style={{
-                          fontSize: '14px',
+                          fontSize: '15px',
                           textAlign: 'center',
                           height: '30px'
                         }}
@@ -145,7 +158,22 @@ function RegisterComponent() {
 
               <div className="p-4 flex bg-ayotaku-box rounded-lg justify-center items-center col-span-12 md:col-span-5 sm:col-span-5">
                 <div className="grid grid-cols-1">
-                  Login Google
+
+                <button 
+                  className="flex items-center justify-center w-full px-4 py-2 bg-white border border-gray-400 shadow-sm dark:text-black hover:bg-gray-100 text-sm rounded-3xl"
+                  onClick={handlerSignUpGoogle}
+                >
+                  {/* ICON SVG Google */}
+                  <svg className="w-6 h-6 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+                    <path fill="#EA4335" d="M24 9.5c3.5 0 6.4 1.2 8.8 3.5l6.5-6.5C34.4 2.5 29.5 0 24 0 14.6 0 6.7 5.8 3 14.1l7.7 6.3C12.3 13.3 17.7 9.5 24 9.5z"></path>
+                    <path fill="#4285F4" d="M46.9 24.6c0-1.7-.1-2.9-.3-4.3H24v8.1h13c-.5 2.4-1.7 4.4-3.5 5.8l7.7 6c4.4-4.1 6.7-10.1 6.7-15.6z"></path>
+                    <path fill="#FBBC05" d="M10.9 28.7c-1.2-2.4-1.9-5-1.9-7.7s.7-5.3 1.9-7.7l-7.7-6.3C2.3 11.3 0 17.4 0 24s2.3 12.7 6.3 17.3l7.6-6.3z"></path>
+                    <path fill="#34A853" d="M24 48c6.5 0 11.9-2.1 15.8-5.7l-7.7-6.1c-2.1 1.5-4.9 2.5-8 2.5-6.3 0-11.6-4.2-13.4-10.1l-7.7 6C6.8 42.1 14.7 48 24 48z"></path>
+                    <path fill="none" d="M0 0h48v48H0z"></path>
+                  </svg>
+                  Sign Up with Google
+                </button>
+
                 </div>
               </div>
             </div>
