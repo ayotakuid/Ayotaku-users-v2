672b58f0bc261fdc1b8a9ca24d280f472ffc5b5b
Rezu0 <yunianrezky031@gmail.com>
2024-08-15 20:22:27 +0700

    component untuk activate account

---
+++ src/component/alert/AlertSuccessComponent.jsx
@@ -0,0 +1,14 @@
+import { Helmet } from "react-helmet-async";
+
+function AlertSuccessComponent() {
+  return (
+    <>
+      <Helmet>
+        <title>Ayotaku id - Cek email</title>
+      </Helmet>
+      <p>Berhasil Register, Silahkan cek Email untuk Aktifasi Account!</p>
+    </>
+  )
+}
+
+export default AlertSuccessComponent;
