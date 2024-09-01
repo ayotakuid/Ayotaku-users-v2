489674b51686b7ba2358d44b1c7c9d1c34110993
Rezu0 <yunianrezky031@gmail.com>
2024-09-01 23:38:45 +0700

    config color dan size tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,21 @@ export default {
         backgroundColor: {
           "ayotaku-box": "#46496A",
           "ayotaku-button": "#D9D2FF",
+          "ayotaku-super-light": "#f6f6f9",
+          "ayotaku-normal-light": "#ecedf2",
+          "ayotaku-light": "#d6d7e1",
+          "ayotaku-bit-dark": "#b2b4c7",
+          "ayotaku-normal-dark": "#888ca8",
+          "ayotaku-dark": "#3b3d51",
+          "ayotaku-super-dark": "#23242e",
         },
         textColor: {
-          "ayotaku-text-default": "#D9D2FF",
+          "ayotaku-text-default": "#d9d2ff",
         },
+        fontSize: {
+          "ayotaku-text-xs": "10px",
+          "ayotaku-text-sm": "12px",
+        }
       },
   },
   plugins: [],
