4cd70804c05d15f2cfd84ff4f1cfff18e218a8f7
Rezu0 <yunianrezky031@gmail.com>
2024-09-08 14:40:10 +0700

    add: main layout untuk page profile settings

---
+++ src/component/ProfileComponent.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { Divider } from "primereact/divider";
+
+// IMPORT COMPONENT
+import SvgRenderComponent from "./utils/SvgRenderComponent";
+import { classNames } from "primereact/utils";
+import { Helmet } from "react-helmet-async";
+
+function ProfileComponent({ isProfileUser }) {
+  const navigate = useNavigate();
+  
+  return (
+    <>
+      <Helmet>
+        <title>Ayotaku.id - Profile</title>
+      </Helmet>
+      <div className="flex flex-col mx-auto px-7 py-5 w-full max-w-screen-xl">
+        <div className="text-2xl sm:text-3xl font-bold">Profile Settings</div>
+        <div className="mt-2 text-ayotaku-text-xs sm:text-ayotaku-text-sm">{`Setting Account mu [ Information Account, Bookmarks Title, Change Password ]`}</div>
+        <Divider />
+
+        
+        <div className="container">
+          <div className="grid grid-cols-1 md:grid-cols-12 gap-2">
+
+            <div className="col-span-1 md:col-span-3 p-1">
+              <div className="grid grid-cols-2 sm:grid-cols-1 gap-2 text-start text-lg">
+                <div 
+                  className={classNames(
+                    location.pathname !== '/profile/me' ? "bg-ayotaku-dark hover:bg-ayotaku-super-dark" : "bg-ayotaku-super-dark",
+                    "col-span-1 hover:duration-500 px-3 py-1 rounded-md cursor-pointer text-base"
+                  )}
+                  onClick={() => navigate('/profile/me')}
+                >
+                  Account
+                </div>
+                <div 
+                  className={classNames(
+                    location.pathname !== '/profile/bookmarks' ? "bg-ayotaku-dark hover:bg-ayotaku-super-dark" : "bg-ayotaku-super-dark",
+                    "col-span-1 hover:duration-500 px-3 py-1 rounded-md cursor-pointer text-base"
+                  )}
+                  onClick={() => navigate('/profile/bookmarks')}
+                >
+                  Bookmarks
+                </div>
+                <div className="col-span-full bg-ayotaku-dark hover:bg-ayotaku-super-dark hover:duration-500 px-3 py-1 rounded-md cursor-pointer text-base">
+                  Change Password
+                </div>
+              </div>
+            </div>
+
+            {/* ISI SEMUA CONTENT AKAN DI RENDER DISINI UNTUK PROFILE SETTINGS */}
+            <div className="col-span-1 md:col-span-9 p-1 px-3 w-full rounded-md">
+              <Outlet />
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    </>
+  )
+}
+
+export default ProfileComponent;
