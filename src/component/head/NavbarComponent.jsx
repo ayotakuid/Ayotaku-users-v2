34fd9571872d4bf81be3b9e7467fb6eee818ec8e
Rezu0 <yunianrezky031@gmail.com>
2024-09-16 14:59:23 +0700

    change: minor change untuk cursor image di navbar

--- src/component/head/NavbarComponent.jsx
+++ src/component/head/NavbarComponent.jsx
@@ -74,7 +74,7 @@ function NavbarComponent({
             </div>
 
             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-              <div className="flex flex-shrink-0 items-center" onClick={() => navigate('/')}>
+              <div className="flex flex-shrink-0 items-center cursor-pointer" onClick={() => navigate('/')}>
                 <img className='h-10 w-auto' src={AyotakuLogo} alt='Ayotaku.id'/>
               </div>
 
