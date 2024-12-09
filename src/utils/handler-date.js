4871f2f58670274197d5231e55029f2e12f1ac73
Rezu0 <yunianrezky031@gmail.com>
2024-12-09 19:39:16 +0700

    add: format date baru

--- src/utils/handler-date.js
+++ src/utils/handler-date.js
@@ -9,3 +9,12 @@ export function formatDateToString(date) {
   //  Minggu, 1 September 2024
   return new Date(date).toLocaleDateString("id-ID", options)
 }
+
+export function formatDateLastUpdate(isoString) {
+  const date = new Date(isoString);
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const year = date.getFullYear();
+
+  return `${day}/${month}/${year}`;
+}
