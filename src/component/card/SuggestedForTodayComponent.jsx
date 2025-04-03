2abca0db682efe60c2ab017f11d353f9c2905781
Rezu0 <yunianrezky031@gmail.com>
2025-04-03 11:34:20 +0700

    add: display rating and airing status in SuggestedForTodayComponent

--- src/component/card/SuggestedForTodayComponent.jsx
+++ src/component/card/SuggestedForTodayComponent.jsx
@@ -22,6 +22,23 @@ function SuggestedForTodayComponent() {
     fetchSuggestedAnime();
   }, [setSuggestedAnime]);
 
+  const checkingRating = (rating) => {
+    return !rating ? 'none' : rating;
+  }
+
+  const checkingAiring = (airing) => {
+    if (airing.indexOf('_') === -1) {
+      return airing;
+    } else {
+      const splitAiring = airing.split('_');
+      const mappingAiring = splitAiring.map((text) => {
+        return text.charAt(0).toUpperCase() + text.slice(1);
+      }).join('');
+
+      return mappingAiring;
+    }
+  }
+
   return (
     <div className="grid grid-cols-12 gap-3 my-10 mx-5 p-2 sm:mx-10 md:mx-20 lg:mx-24">
         <div className="col-span-12">
@@ -38,12 +55,25 @@ function SuggestedForTodayComponent() {
               key={items.dataNimes.id_anime} 
               className='col-span-6 md:col-span-2 sm:col-span-3 gap-1 mb-2 w-auto max-w-xs overflow-hidden group'
             >
-              <img 
-                src={items.dataNimes.animes.foto_anime} 
-                alt={items.dataNimes.animes.nama_anime.romanji} 
-                className='w-72 rounded-lg object-cover hover:opacity-50 cursor-pointer duration-300'
-                loading="lazy"
-              />
+              <div className="relative w-full">
+                <img 
+                  src={items.dataNimes.animes.foto_anime} 
+                  alt={items.dataNimes.animes.nama_anime.romanji} 
+                  className='w-72 rounded-lg object-cover hover:opacity-50 cursor-pointer duration-300'
+                  loading="lazy"
+                />
+
+                <span className="flex items-center absolute top-2 right-2 w-fit bg-black bg-opacity-50 text-ayotaku-text-default px-2 py-1 rounded-md text-ayotaku-text-sm gap-1 group-hover:opacity-70 duration-500">
+                <svg className="w-4 h-4 text-yellow-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                  <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z"/>
+                </svg>
+                  <span className="text-ayotaku-text-default">{checkingRating(items.dataNimes.animes.rating)}</span>
+                </span>
+
+                <span className="flex items-center absolute bottom-2 left-2 w-fit bg-black bg-opacity-50 text-ayotaku-text-default px-2 py-1 rounded-md text-ayotaku-text-sm gap-1 group-hover:opacity-70 duration-500">
+                  <span className="text-ayotaku-text-default">{checkingAiring(items.dataNimes.animes.status.statusAiring)}</span>
+                </span>
+              </div>
               
               <div className="py-1">
                 <h3 
