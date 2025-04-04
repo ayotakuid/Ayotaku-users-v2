e1779e20b62e62e62a6b8afe5f9583a1ec41dc5b
Rezu0 <yunianrezky031@gmail.com>
2025-04-04 20:53:47 +0700

    feat: implement pagination and display for anime cards in AnimeHomeComponent

--- src/component/AnimeHomeComponent.jsx
+++ src/component/AnimeHomeComponent.jsx
@@ -1,6 +1,35 @@
+import { useEffect, useState } from "react";
 import AnimeCardHomeComponent from "./card/AnimeCardHomeComponent";
+import { handlerFetchingAnimesPagination } from "../utils/handler-fetching-animes";
+import { toast } from "sonner";
+import { useSearchParams } from "react-router-dom";
 
 function AnimeHomeComponent() {
+  const [isAnimesPagination, setAnimesPagination] = useState([]);
+  const [isSearchParams, setSearchParams] = useSearchParams();
+
+  const fetchAnimesPagination = async () => {
+    try {
+      const page = isSearchParams.get('page') ?? 1;
+      const limit = 18;
+      const response = await handlerFetchingAnimesPagination({ page: page, limit: limit });
+
+      if (response?.data.length === 0) {
+        toast.error('Data Anime Kosong!');
+        return;
+      }
+
+      setAnimesPagination(response?.data);
+    } catch (err) {
+      console.error(err);
+      toast.error('Error Load data Anime!');
+    }
+  }
+
+  useEffect(() => {
+    fetchAnimesPagination();
+  }, [setAnimesPagination, isSearchParams]);
+
   return (
     <div className="grid grid-cols-1 mx-10 my-5 md:mx-20 md:my-10">
       <div className="flex flex-wrap items-center justify-between gap-2 bg-ayotaku-dark rounded-md px-3 py-2 uppercase text-md md:text-lg mb-10">
@@ -32,7 +61,9 @@ function AnimeHomeComponent() {
 
 
       <div className="grid grid-cols-12 my-3 justify-center gap-2">
-        <AnimeCardHomeComponent />
+        <AnimeCardHomeComponent 
+          animes={isAnimesPagination}
+        />
       </div>
     </div>
   );
--- src/component/card/AnimeCardHomeComponent.jsx
+++ src/component/card/AnimeCardHomeComponent.jsx
@@ -1,36 +1,64 @@
-function AnimeCardHomeComponent() {
+import { useEffect, useState } from "react";
+
+function AnimeCardHomeComponent({ animes }) {
+  const [isAnimes, setAnimes] = useState([]);
+
+  useEffect(() => {
+    setAnimes(animes);
+  }, [animes]); // cukup depend ke `animes` saja
+
+  const checkingRating = (rating) => {
+    return !rating ? 'none' : rating;
+  }
+
+  const checkingAiring = (airing) => {
+    if (airing.indexOf('_') === -1) {
+      return airing;
+    } else {
+      const splitAiring = airing.split('_');
+      const mappingAiring = splitAiring.map((text) => {
+        return text.charAt(0).toUpperCase() + text.slice(1);
+      }).join('');
+
+      return mappingAiring;
+    }
+  }
+
   return (
-    <div 
-      className='col-span-6 sm:col-span-3 md:col-span-3 lg:col-span-2 gap-1 mb-2 w-auto max-w-xs overflow-hidden group'
-    >
-      <div className="relative w-full">
-        <img 
-          src="https://cdn.myanimelist.net/images/anime/1922/134324l.jpg" 
-          // alt={items.dataNimes.animes.nama_anime.romanji} 
-          className='w-56 rounded-lg object-cover hover:opacity-50 cursor-pointer duration-300'
-          loading="lazy"
-        />
-
-        <span className="flex items-center absolute top-2 right-2 w-fit bg-black bg-opacity-50 text-ayotaku-text-default px-2 py-1 rounded-md text-ayotaku-text-sm gap-1 group-hover:opacity-70 duration-500">
-          <svg className="w-4 h-4 text-yellow-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-            <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z"/>
-          </svg>
-          <span className="text-ayotaku-text-default">asd</span>
-        </span>
-
-        <span className="flex items-center absolute bottom-2 left-2 w-fit bg-black bg-opacity-50 text-ayotaku-text-default px-2 py-1 rounded-md text-ayotaku-text-sm gap-1 group-hover:opacity-70 duration-500">
-          <span className="text-ayotaku-text-default">asd</span>
-        </span>
-      </div>
-              
-      <div className="py-1">
-        <h3 
-          className="text-md font-semibold text-ayotaku-text-default line-clamp-2 group-hover:underline duration-300 cursor-pointer"
+    <>
+      {isAnimes.map((items, index) => (
+        <div 
+          key={items.uuid}
+          className='col-span-6 sm:col-span-3 md:col-span-3 lg:col-span-2 gap-1 mb-2 w-auto max-w-xs overflow-hidden group'
         >
-          Tokidoki Bosotto Russia-go de Dereru Tonari no Alya-san
-        </h3>
-      </div>
-    </div>
+          <div className="relative w-full">
+            <img 
+              src={items.data.foto_anime} 
+              alt={items.data.nama_anime.romanji} 
+              className='w-full rounded-lg object-cover hover:opacity-50 cursor-pointer duration-300'
+              loading="lazy"
+            />
+
+            <span className="flex items-center absolute top-2 right-2 w-fit bg-black bg-opacity-50 text-ayotaku-text-default px-2 py-1 rounded-md text-ayotaku-text-sm gap-1 group-hover:opacity-70 duration-500">
+              <svg className="w-4 h-4 text-yellow-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z"/>
+              </svg>
+              <span className="text-ayotaku-text-default">{checkingRating(items.data.rating)}</span>
+            </span>
+
+            <span className="flex items-center absolute bottom-2 left-2 w-fit bg-black bg-opacity-50 text-ayotaku-text-default px-2 py-1 rounded-md text-ayotaku-text-sm gap-1 group-hover:opacity-70 duration-500">
+              <span className="text-ayotaku-text-default">{checkingAiring(items.data.status.statusAiring)}</span>
+            </span>
+          </div>
+                  
+          <div className="py-1">
+            <h3 className="text-md font-semibold text-ayotaku-text-default line-clamp-2 group-hover:underline duration-300 cursor-pointer">
+              {items.data.nama_anime.romanji}
+            </h3>
+          </div>
+        </div>
+      ))}
+    </>
   )
 }
 
