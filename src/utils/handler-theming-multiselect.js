195294e438385ff9894870de7579498d8b6475c0
Rezu0 <yunianrezky031@gmail.com>
2025-04-12 13:50:02 +0700

    fix: enhance transition effects for multiselect component

--- src/utils/handler-theming-multiselect.js
+++ src/utils/handler-theming-multiselect.js
@@ -4,7 +4,7 @@ export const TRANSITIONS = {
   overlay: {
       timeout: 150,
       classNames: {
-          enter: 'opacity-0 scale-75',
+          enter: 'opacity-0 scale-75 transition-all duration-150',
           enterActive: 'opacity-100 !scale-100 transition-transform transition-opacity duration-150 ease-in',
           exit: 'opacity-100',
           exitActive: '!opacity-0 transition-opacity duration-150 ease-linear'
