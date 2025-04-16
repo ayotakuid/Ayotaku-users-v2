cd74538755b830c62e938a2be6ed8ec6a73027fc
Rezu0 <yunianrezky031@gmail.com>
2025-04-16 13:31:40 +0700

    feat: replace SVG icon with image in ButtonScrollUpComponent and adjust button styling

--- src/component/utils/ButtonScrollUpComponent.jsx
+++ src/component/utils/ButtonScrollUpComponent.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import PointinUpAnime from '../../image/pointing-up.png';
 
 function ButtonScrollUpComponent() {
   const [isVisible, setIsVisible] = useState(false);
@@ -24,11 +25,9 @@ function ButtonScrollUpComponent() {
   return (
     <button
       onClick={scrollToTop}
-      className={`fixed bottom-5 right-5 p-3 bg-ayotaku-box hover:bg-ayotaku-super-dark text-ayotaku-text-default rounded-full shadow-lg duration-500 cursor-pointer transition-opacity ${isVisible ? "opacity-100" : "opacity-0"}`}
+      className={`fixed bottom-0 right-3 md:right-20 p-3 text-ayotaku-text-default rounded-t-md duration-500 cursor-pointer transition-opacity ${isVisible ? "opacity-100" : "opacity-0"}`}
     >
-      <svg className="w-6 h-6 text-gray-800 dark:text-ayotaku-text-default" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m5 15 7-7 7 7"/>
-      </svg>
+      <img src={PointinUpAnime} alt="Pointing up Anime" className="w-16 md:w-24 bg-transparent rounded-full" />
     </button>
   )
 }
