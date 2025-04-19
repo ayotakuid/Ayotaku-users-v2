e4d6cf83cc880df77b04d876ff9143b58084ae89
Rezu0 <yunianrezky031@gmail.com>
2025-04-19 20:48:29 +0700

    feat: add custom animation and keyframes for enhanced UI transitions

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,6 +34,18 @@ export default {
         },
         ringColor: {
           "ayotaku-ring": "#3b3d51",
+        },
+        height: {
+          "ayotaku-height": "38rem",
+        },
+        animation: {
+          'fade-in': 'fadeIn 0.8s ease-out forwards',
+        },
+        keyframes: {
+          fadeIn: {
+            '0%': { opacity: 0, transform: 'translateY(20px)' },
+            '100%': { opacity: 1, transform: 'translateY(0)' },
+          }
         }
       },
     container: {
