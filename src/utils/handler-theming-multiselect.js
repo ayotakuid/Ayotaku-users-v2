2584f5ef936ff55c70f3638e8cb0dbe6bcfbd7d7
Rezu0 <yunianrezky031@gmail.com>
2025-04-19 14:19:52 +0700

    refactor: simplify enter transition class in TRANSITIONS for cleaner styling

--- src/utils/handler-theming-multiselect.js
+++ src/utils/handler-theming-multiselect.js
@@ -1,10 +1,10 @@
 import { classNames } from "primereact/utils";
 
-export const TRANSITIONS = {
+const TRANSITIONS = {
   overlay: {
       timeout: 150,
       classNames: {
-          enter: 'opacity-0 scale-75 transition-all duration-150',
+          enter: 'opacity-0 scale-75',
           enterActive: 'opacity-100 !scale-100 transition-transform transition-opacity duration-150 ease-in',
           exit: 'opacity-100',
           exitActive: '!opacity-0 transition-opacity duration-150 ease-linear'
